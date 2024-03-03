d425eb3653746fde8de8e876182243c4f976755f
sauhits <atsushi.obata1207@gmail.com>
2024-03-03 15:35:26 +0900

    reflesh

--- GameMain.java
+++ GameMain.java
@@ -4,10 +4,10 @@
 public class GameMain {
 
     public static void main(String[] args) {
-        new ArrayDB();
-        while (!Logic.endCheck()) {
+        while (true) {
+            // 座標の指定
             Logic.chooseCoordinate();
+            ArrayDB.setWBarray(Logic.chooseX, Logic.chooseY, Logic.nowStone);
         }
-
     }
 }
